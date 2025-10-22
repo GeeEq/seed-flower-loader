d92c05329e7d74336b916b0733c93f66df051d12
GeeEq <sielaleis@gmail.com>
2025-10-22 16:37:39 +0300

    packages installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import SeedAndFlowerLoader from "./SeedAndFlowerLoader";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <SeedAndFlowerLoader />
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      backgroundColor: "#111827"
+    }}>
+      <SeedAndFlowerLoader size={100} />
     </div>
   );
 }
-
-export default App;
