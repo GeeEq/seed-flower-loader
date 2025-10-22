97e5aa2225558ef2c50d07e1a576ec7877469a78
GeeEq <sielaleis@gmail.com>
2025-10-22 16:15:55 +0300

    Initial commit: Seed and Flower Loader

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SeedAndFlowerLoader from "./SeedAndFlowerLoader";
+
+function App() {
+  return (
+    <div className="App">
+      <SeedAndFlowerLoader />
+    </div>
+  );
+}
+
+export default App;
